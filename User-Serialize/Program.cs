4294e3c85954c1b0ea7148f18b4432c1d880bc52
Agil Bashirov <71183628+AgilBashirov@users.noreply.github.com>
2021-11-12 21:54:33 +0400

    Add files via upload

---
+++ User-Serialize/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace User_Serialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            //User user = new User();
+            //user.Name = "Martin";
+            //user.Surname = "Heidegger";
+            //user.Age = 77;
+
+            //Serialize(user, "Martin");
+
+            //Console.WriteLine(Deserialize("Martin"));
+            //List<string> strList = new List<string>();
+            //strList.Add("kdk");
+            //strList.Add("kdkjdj");
+            //strList.Add("kdkkdckdh");
+
+            string path = @"C:\Users\agilab\Desktop\Martin.txt";
+            //WriteAll(path, strList);
+            foreach (var item in ReadAll(path))
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+        public static void Serialize(User user, string filename)
+        {
+            string path = @"C:\Users\agilab\Desktop\" + filename + ".txt";
+
+            Stream stream = new FileStream(path, FileMode.Create);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(stream, user);
+        }
+        public static User Deserialize(string filename)
+        {
+            string path = @"C:\Users\agilab\Desktop\" + filename + ".txt";
+            Stream stream = new FileStream(path, FileMode.Open);
+            BinaryFormatter bf = new BinaryFormatter();
+            var data = (User)bf.Deserialize(stream);
+            return data;
+        }
+
+        public static void WriteAll(string path, List<string> arr)
+        {
+            File.WriteAllLines(path, arr);
+        }
+
+        public static List<string> ReadAll(string path)
+        {
+            List<string> newList = new List<string>();
+
+            foreach (var item in File.ReadAllLines(path))
+            {
+                newList.Add(item);
+            }
+            return newList;
+        }
+    }
+}
---
+++ User-Serialize/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace User_Serialize
+{
+    [Serializable]
+    class User : ISerializable
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public byte Age { get; set; }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Name", Name, typeof(string));
+            info.AddValue("Surname", Surname, typeof(string));
+            info.AddValue("Age", Age, typeof(byte));
+        }
+        public User()
+        {
+
+        }
+        public User(SerializationInfo info, StreamingContext context)
+        {
+            Name = (string)info.GetValue("Name", typeof(string));
+            Surname = (string)info.GetValue("Surname", typeof(string));
+            Age = (byte)info.GetValue("Age", typeof(byte));
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Surname} - Age:{Age}";
+        }
+    }
+}
